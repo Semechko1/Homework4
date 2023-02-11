b5216f49718f64712e6a9711bce72df28c4e2611
Semka <96279531+Semechko1@users.noreply.github.com>
2023-02-11 13:07:58 +0200

    Main Homework

---
+++ main.py
@@ -0,0 +1,19 @@
+class Coder1:
+    def __init__(self):
+        self.a = int(input("Number A - "))
+        self.b = int(input("Number B - "))
+
+    def ret_numb(self):
+        c = int(self.a + self.b)
+        d = int(self.a - self.b)
+        return c + d
+
+
+class Coder2:
+    def __init__(self, c):
+        self.d = c / 2
+        print(self.d)
+
+
+math = Coder1()
+math2 = Coder2(math.ret_numb())
